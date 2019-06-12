ad3b9147af40e29c27c6cde2ab1a5e5152fe714c
Arash Hatami <hatamiarash7@gmail.com>
2019-06-12 20:55:01 +0430

    add first function

--- src/LaravelUtils.php
+++ src/LaravelUtils.php
@@ -4,5 +4,14 @@ namespace Hatamiarash7\Utils;
 
 class LaravelUtils
 {
+	public function removeStrings($words, $string)
+	{
+		foreach ($words as $word) {
+			$string = str_replace($word, "", $string);
+		}
 
+		$string = str_replace("  ", " ", $string);
+
+		return $string;
+	}
 }
